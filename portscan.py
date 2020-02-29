0a5a166b9c8bf41d449254613da6f31aca8c3b6f
Ricardo Varela <48896758+ricardomvv@users.noreply.github.com>
2020-02-29 12:37:04 -0300

    Update portscan.py

--- portscan.py
+++ portscan.py
@@ -2,6 +2,6 @@ import socket
 
 cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 cliente.connect(('bancocn.com', 80))
-cliente.send('ausaushahusahu\n\n')
+cliente.send('mensagemexibida\n\n')
 resposta = cliente.recv(1024)
 print resposta
